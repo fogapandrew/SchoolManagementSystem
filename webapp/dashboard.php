5209a02b432e53a4195b5e77faeb18b758535d63
Njinju Fogap <andrewfogap@gmail.com>
2024-04-13 19:34:41 +0200

    cleaing up pages

--- webapp/dashboard.php
+++ webapp/dashboard.php
@@ -14,21 +14,9 @@
     <br>
     <br>
     <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    <div style="display: flex; justify-content: center;">
+          <iframe src="https://app.powerbi.com/discover/gallery/653426d8-503a-4a10-8a02-71854968d73a?experience=power-bi" width="1200" height="800"></iframe>
+    </div>
     <br>
     <br>
     <br>
